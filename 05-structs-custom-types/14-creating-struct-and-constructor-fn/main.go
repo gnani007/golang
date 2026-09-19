88e5747f7171c2cdf78bc7b4f4d7d9bc89dcc060
apple <apple@localhost>
2026-09-20 00:59:53 +0530

    added structs and writing the notes

--- 05-structs-custom-types/14-creating-struct-and-constructor-fn/main.go
+++ 05-structs-custom-types/14-creating-struct-and-constructor-fn/main.go
@@ -1,43 +1,37 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+
+	"example.com/note/note"
 )
 
 func main() {
-	title, content, err := getNoteData()
+	title, content := getNoteData()
+
+	userNote, err := note.New(title, content)
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	fmt.Println(userNote)
 }
 
-func getNoteData() (string, string, error) {
-	title, err := getUserInput("Note title:")
+func getNoteData() (string, string) {
+	title := getUserInput("Enter note title: ")
+	content := getUserInput("Enter content: ")
 
-	if err != nil {
-		return "", "", err
-	}
-
-	content, err := getUserInput("Note content:")
-
-	if err != nil {
-		return "", "", err
-	}
-	// returns the title and content
-	return title, content, nil
+	return title, content
 }
 
-func getUserInput(prompt string) (string, error) {
+func getUserInput(prompt string) string {
 	fmt.Print(prompt)
 	var value string
+
 	fmt.Scanln(&value)
 
-	if value == "" {
-		return "", errors.New("Invalid input.")
-	}
+	return value
 
-	return value, nil
 }
--- 05-structs-custom-types/15-handling-long-user-input/note/note.go
+++ 05-structs-custom-types/15-handling-long-user-input/note/note.go
@@ -2,6 +2,7 @@ package note
 
 import (
 	"errors"
+	"fmt"
 	"time"
 )
 
@@ -11,9 +12,13 @@ type Note struct {
 	createdAt time.Time
 }
 
+func (note Note) Display() {
+	fmt.Printf("Your note title %v has the following content:\n\n%v\n\n", note.title, note.content)
+}
+
 func New(title, content string) (Note, error) {
 	if title == "" || content == "" {
-		return Note{}, errors.New("Invalid input.")
+		return Note{}, errors.New("Invalid input")
 	}
 
 	return Note{
